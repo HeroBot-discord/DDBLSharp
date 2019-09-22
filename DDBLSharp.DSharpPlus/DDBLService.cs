34897ec6d05b80e3d73c1f29c4ca3e82adb3d139
Matthieu <matthieu974fr.pignolet@gmail.com>
2019-09-23 01:01:56 +0400

    Revert "Project cleanup"
    This reverts commit 8068e0d4be8028164b2dc08ac8ed60f8f644d153.

---
+++ DDBLSharp.DSharpPlus/DDBLService.cs
@@ -0,0 +1,57 @@
+﻿using DSharpPlus;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace DDBLSharp.DSharpPlus
+{
+    public class DDBLService
+    {
+        /// <summary>
+        /// The client ussed to connect to the api.
+        /// </summary>
+        public DDBLBotClient Client { get; private set; }
+        /// <summary>
+        /// The configuration of the client.
+        /// </summary>
+        private readonly ClientConfiguration _config;
+        /// <summary>
+        /// The Discord.NEt Wrapper
+        /// </summary>
+        private readonly DiscordClientWrapper _discord;
+
+        /// <summary>
+        /// Used to create the service.
+        /// </summary>
+        public DDBLService(DiscordShardedClient client, ClientConfiguration configuration)
+        {
+            _config = configuration;
+            _discord = new DiscordShardedClientWrapper(ref client);
+            client.GuildAvailable += async (guild) => await UpdateStats();
+            client.GuildDeleted += async (guild) => await UpdateStats();
+            client.Ready += async (args) => await Init();
+        }
+        public Task UpdateStats()
+        {
+            return Client.UpdateStatsAsync(new DDBLClient.User.Stats()
+            {
+                ServerCount = _discord.GuildCount,
+                ShardCount = _discord.ShardCount
+            });
+        }
+        public DDBLService(DiscordClient client,ClientConfiguration configuration) {
+            _discord = new DiscordSocketClientWrapper(ref client);
+            _config = configuration;
+            client.Ready += async (shard) => await Init();
+        }
+        private async Task Init()
+        {
+
+            // Now the client is lunched.
+            Client = new DDBLBotClient() {
+                ApiKey = _config.ApiKey,
+                BotId = _discord.Id
+            };
+            await UpdateStats();
+        }
+    }
+}
